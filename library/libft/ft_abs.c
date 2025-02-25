71cd8571930a44b2c20ff052be659510f1d281a6
Víctor J. Goyzueta <167643295+victor-goyzueta@users.noreply.github.com>
2025-02-25 21:54:41 +0100

    update

--- library/libft/ft_abs.c
+++ library/libft/ft_abs.c
@@ -1,13 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_abs.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vgoyzuet <vgoyzuet@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/25 21:52:43 by vgoyzuet          #+#    #+#             */
+/*   Updated: 2025/02/25 21:53:52 by vgoyzuet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-int ft_abs(int x)
+int	ft_abs(int x)
 {
-    if (x < 0)
-    {
-        if (x == INT_MIN)
-            return INT_MAX;
-        return (-x);
-    }
-    else
-        return (x);
+	if (x < 0)
+	{
+		if (x == INT_MIN)
+			return (INT_MAX);
+		return (-x);
+	}
+	else
+		return (x);
 }
